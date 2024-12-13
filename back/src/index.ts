d16fc9c9731aaf38cf0189d44c1d8e156dc6e9f3
Agustin Haag <agustin-haag@hotmail.com>
2024-12-13 18:55:05 -0300

    Correccion puerto

--- back/src/index.ts
+++ back/src/index.ts
@@ -4,13 +4,12 @@ import "dotenv/config";
 import { PORT } from "./config/envs";
 import { AppDataSource } from "./config/dataSource";
 
-
 const initialize = async () => {
-    await AppDataSource.initialize();
-    console.log("Database initialized");
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
+  await AppDataSource.initialize();
+  console.log("Database initialized");
+  app.listen(PORT || 4000, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
 initialize();
