1de789aced0fc3b0bd2e834e543a2484dda7bdd3
Agustin Haag <agustin-haag@hotmail.com>
2025-01-30 18:56:21 -0300

    Correcciones

--- front/src/components/devices/ServiceDevice.tsx
+++ front/src/components/devices/ServiceDevice.tsx
@@ -14,6 +14,7 @@ const ServiceDevice: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const url = process.env.NEXT_PUBLIC_URL!;
   const token: string = useSelector((state: RootState) => state.user.token);
+  console.log(infoDevice);
   useEffect(() => {
     const intervalId = setInterval(() => {
       fetchInfoDevice(dataUser?.id, url, token)
@@ -96,105 +97,114 @@ const ServiceDevice: React.FC = () => {
             ) : (
               <>
                 {/* Si tienes un dispositivo */}
-                {dataUser.tokenwablas && deviceStatus !== "disconnected" ? (
+                {dataUser.tokenwablas ? (
                   // Si tienes el tokenwablas, mostrar la información del dispositivo y el texto de los mensajes
                   <div className="text-custom-white ">
-                    <div className="flex flex-col gap-2 mb-1">
-                      <h2 className="sm:text-2xl text-lg">IMPORTANTE:</h2>
-                      <p className="text-neutral-400 sm:text-base text-sm">
-                        Inicialmente, dispone de 5000 mensajes gratuitos de
-                        forma mensual.
-                      </p>
-                      <p className="text-neutral-400 sm:text-base text-sm">
-                        Actualmente, el proceso de envío de mensajes es lento.
-                        Una mayor velocidad podría ser interpretada como spam
-                        por la empresa WhatsApp, resultando en el bloqueo del
-                        número de teléfono. Para evitar esta situación, el envío
-                        es de 1 minuto por mensaje.
-                      </p>
-                      <p className="text-neutral-400 sm:text-base text-sm">
-                        Por lo tanto, este software no debe utilizarse para
-                        informar emergencias a los clientes (por ejemplo, la
-                        caída imprevista de equipos). Su uso está diseñado para
-                        comunicaciones anticipadas, como avisos de vencimiento,
-                        mantenimientos programados, etc.
-                      </p>
-                    </div>
-                    {infoDevice && infoDevice.status && (
+                    {infoDevice && (
                       // Si tienes el dispositivo y el estado, mostrar la información
-                      <div className="flex lg:flex-row flex-col gap-5 w-full my-3">
-                        <div className="mb-2 min-w-80">
-                          <div className="flex flex-col gap-1">
-                            <h2 className="text-2xl mb-2">
-                              Información del dispositivo
-                            </h2>
-                            <p className="text-base text-neutral-400">
-                              <span className="text-lg text-custom-white">
-                                Dispositivo:{" "}
-                              </span>
-                              {infoDevice.data.name}
+                      <>
+                        {deviceStatus !== "disconnected" && (
+                          <div className="flex flex-col gap-2 mb-1">
+                            <h2 className="sm:text-2xl text-lg">IMPORTANTE:</h2>
+                            <p className="text-neutral-400 sm:text-base text-sm">
+                              Inicialmente, dispone de 5000 mensajes gratuitos
+                              de forma mensual.
                             </p>
-                            <p className="text-base text-neutral-400">
-                              <span className="text-lg mr-1 text-custom-white">
-                                Número:
-                              </span>
-                              {infoDevice.data.sender}
+                            <p className="text-neutral-400 sm:text-base text-sm">
+                              Actualmente, el proceso de envío de mensajes es
+                              lento. Una mayor velocidad podría ser interpretada
+                              como spam por la empresa WhatsApp, resultando en
+                              el bloqueo del número de teléfono. Para evitar
+                              esta situación, el envío es de 1 minuto por
+                              mensaje.
                             </p>
-                            <p
-                              className={`text-base ${
-                                deviceStatus === "disconnected"
-                                  ? "text-red-600"
-                                  : "text-green-600"
-                              }`}
-                            >
-                              <span className="text-lg text-custom-white">
-                                Estado:
-                              </span>
-                              {deviceStatus === "disconnected"
-                                ? " Desconectado"
-                                : " Conectado"}
+                            <p className="text-neutral-400 sm:text-base text-sm">
+                              Por lo tanto, este software no debe utilizarse
+                              para informar emergencias a los clientes (por
+                              ejemplo, la caída imprevista de equipos). Su uso
+                              está diseñado para comunicaciones anticipadas,
+                              como avisos de vencimiento, mantenimientos
+                              programados, etc.
                             </p>
-                            <p className="text-base text-neutral-400">
-                              <span className="text-lg text-custom-white">
-                                Venciminento:{" "}
-                              </span>
-                              {infoDevice.data.expired_date}
-                            </p>
-                            <p className="text-base text-neutral-400">
-                              <span className="text-lg text-custom-white">
-                                Mensajes disponibles:{" "}
-                              </span>
-                              {infoDevice.data.quota}
-                            </p>
-                          </div>
-                        </div>
-                        {deviceStatus === "disconnected" && (
-                          <div className="text-custom-white flex flex-col gap-1">
-                            <h2 className="text-xl">¡Dispositivo validado! </h2>
-                            <p>Procedimiento:</p>
-                            <ul className="list-decimal ml-4">
-                              <li>
-                                En el teléfono, abra la aplicación WhatsApp
-                                Business.
-                              </li>
-                              <li>
-                                En WhatsApp Business, active la función de
-                                escaneo QR.
-                              </li>
-                              <li>
-                                En esta página, presione el botón Generar código
-                                QR.
-                              </li>
-                              <li>
-                                Con su teléfono, escanee el código QR generado
-                                en esta página. Una vez escaneado el código QR,
-                                su teléfono estará habilitado para enviar
-                                mensajes masivos a sus clientes.
-                              </li>
-                            </ul>
                           </div>
                         )}
-                      </div>
+
+                        <div className="flex lg:flex-row flex-col gap-5 w-full my-3">
+                          <div className="mb-2 min-w-80">
+                            <div className="flex flex-col gap-1">
+                              <h2 className="text-2xl mb-2">
+                                Información del dispositivo
+                              </h2>
+                              <p className="text-base text-neutral-400">
+                                <span className="text-lg text-custom-white">
+                                  Dispositivo:{" "}
+                                </span>
+                                {infoDevice.data.name}
+                              </p>
+                              <p className="text-base text-neutral-400">
+                                <span className="text-lg mr-1 text-custom-white">
+                                  Número:
+                                </span>
+                                {infoDevice.data.sender}
+                              </p>
+                              <p
+                                className={`text-base ${
+                                  deviceStatus === "disconnected"
+                                    ? "text-red-600"
+                                    : "text-green-600"
+                                }`}
+                              >
+                                <span className="text-lg text-custom-white">
+                                  Estado:
+                                </span>
+                                {deviceStatus === "disconnected"
+                                  ? " Desconectado"
+                                  : " Conectado"}
+                              </p>
+                              <p className="text-base text-neutral-400">
+                                <span className="text-lg text-custom-white">
+                                  Venciminento:{" "}
+                                </span>
+                                {infoDevice.data.expired_date}
+                              </p>
+                              <p className="text-base text-neutral-400">
+                                <span className="text-lg text-custom-white">
+                                  Mensajes disponibles:{" "}
+                                </span>
+                                {infoDevice.data.quota}
+                              </p>
+                            </div>
+                          </div>
+                          {deviceStatus === "disconnected" && (
+                            <div className="text-custom-white flex flex-col gap-1">
+                              <h2 className="text-xl">
+                                ¡Dispositivo validado!{" "}
+                              </h2>
+                              <p>Procedimiento:</p>
+                              <ul className="list-decimal ml-4">
+                                <li>
+                                  En el teléfono, abra la aplicación WhatsApp
+                                  Business.
+                                </li>
+                                <li>
+                                  En WhatsApp Business, active la función de
+                                  escaneo QR.
+                                </li>
+                                <li>
+                                  En esta página, presione el botón Generar
+                                  código QR.
+                                </li>
+                                <li>
+                                  Con su teléfono, escanee el código QR generado
+                                  en esta página. Una vez escaneado el código
+                                  QR, su teléfono estará habilitado para enviar
+                                  mensajes masivos a sus clientes.
+                                </li>
+                              </ul>
+                            </div>
+                          )}
+                        </div>
+                      </>
                     )}
                   </div>
                 ) : (
